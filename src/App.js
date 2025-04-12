70371d8e96643d8a5d5949f09b5bbd2da7c4c1aa
muhammed1 <aitmamatovmuhammed03@gmail.com>
2025-04-12 18:07:42 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
         <Routes>
           <Route path="/" element={<RegisterForm />} />
           <Route path="/dashboard" element={<TrelloBoard />} />
-          {/* <Route path="/main" element={<Main />} /> */}
+          <Route path="/main" element={<Main />} />
         </Routes>
       </BrowserRouter>
     </div>
